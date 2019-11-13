8e3c207564bc44ade96a98f0dd9c301c2620cee7
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2019-11-13 15:31:15 +0300

    git init

---
+++ src/components/RepoSearchList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h1>REPO LIST</h1>
+    <input
+      type="text"
+      name="gh-username"
+      placeholder="Search for a GitHub username..."
+      v-model="username"
+      @keydown.13="search"
+      :class="{ 'has-error': error }"
+    />
+    <span class="loading" v-if="loading">Searching GitHub for "{{ username }}"...</span>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "RepoSearchList",
+  props: {
+    msg: String
+  },
+  data: () => ({
+    username: "",
+    results: "",
+    error: "",
+    loading: false
+  }),
+  watch: {
+    username: function(val) {
+      this.error = "";
+    }
+  },
+  search: function() {
+    if (this.username) {
+      this.loading = true;
+      console.log("Searching for... " + this.username);
+      axios
+        .get("https://api.github.com/users/" + this.username + "/repos")
+        .then(response => {
+          console.log(response.data);
+
+          this.results = response.data;
+          this.error = "";
+
+          this.loading = false;
+        })
+        .catch(error => {
+          this.results = "";
+          this.error = error;
+
+          this.loading = false;
+        });
+    } else {
+      this.results = "";
+      this.error = "";
+
+      this.loading = false;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RepoSearchList />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import RepoSearchList from "@/components/RepoSearchList.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    RepoSearchList
   }
-}
+};
 </script>
