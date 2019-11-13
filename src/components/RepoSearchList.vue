81c9959b2a9f64309c8694cc225a960e6a47710e
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2019-11-13 15:50:53 +0300

    initial commit

--- src/components/RepoSearchList.vue
+++ src/components/RepoSearchList.vue
@@ -31,31 +31,33 @@ export default {
       this.error = "";
     }
   },
-  search: function() {
-    if (this.username) {
-      this.loading = true;
-      console.log("Searching for... " + this.username);
-      axios
-        .get("https://api.github.com/users/" + this.username + "/repos")
-        .then(response => {
-          console.log(response.data);
+  methods: {
+    search: function() {
+      if (this.username) {
+        this.loading = true;
+        console.log("Searching for... " + this.username);
+        axios
+          .get("https://api.github.com/users/" + this.username + "/repos")
+          .then(response => {
+            console.log(response.data);
 
-          this.results = response.data;
-          this.error = "";
+            this.results = response.data;
+            this.error = "";
 
-          this.loading = false;
-        })
-        .catch(error => {
-          this.results = "";
-          this.error = error;
+            this.loading = false;
+          })
+          .catch(error => {
+            this.results = "";
+            this.error = error;
 
-          this.loading = false;
-        });
-    } else {
-      this.results = "";
-      this.error = "";
+            this.loading = false;
+          });
+      } else {
+        this.results = "";
+        this.error = "";
 
-      this.loading = false;
+        this.loading = false;
+      }
     }
   }
 };
