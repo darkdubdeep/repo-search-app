1c7c85da0551b4c0e58d0a736e1e1e78574b9481
Ильнур Ибрагимов <ilnuribragimov@Mac-mini-Ilnur.Dlink>
2019-11-15 20:15:24 +0300

    Refactoring

--- src/components/RepoSearchList.vue
+++ src/components/RepoSearchList.vue
@@ -5,14 +5,11 @@
         type="text"
         name="gh-username"
         placeholder="Search for a GitHub repository by username"
-        v-model="username"
+        :value="username"
         @keyup="search"
         class="search-input"
       />
-      <ul
-        v-show="!error && !loading && username.length > 0"
-        class="dropdown-list"
-      >
+      <ul v-show="!error && !loading" class="dropdown-list">
         <li v-for="item in repoData" :key="item.id" class="dropdown-list__item">
           <router-link
             :to="{ path: `repository/${item.name}` }"
@@ -23,7 +20,9 @@
       </ul>
     </div>
     <h4 v-if="error">{{ error }}</h4>
-    <h4 v-if="!error && !loading && repoData.length < 1">No repositories</h4>
+    <h4 v-if="!error && !loading && repoData.length < 1 && username.length > 0">
+      No repositories
+    </h4>
   </div>
 </template>
 
@@ -32,23 +31,22 @@ import { mapState } from 'vuex';
 export default {
   name: 'RepoSearchList',
   data: () => ({
-    username: '',
     timeOut: null
   }),
   computed: {
-    ...mapState(['loading', 'repoData', 'error'])
+    ...mapState(['loading', 'repoData', 'error', 'username'])
   },
   methods: {
-    search: function() {
-      if (this.username) {
+    search: function(e) {
+      if (e.target.value) {
         this.$store.commit('setLoading', true);
         this.$store.commit('setError', null);
+        this.$store.commit('setUser', e.target.value);
         if (this.timeOut) {
           clearTimeout(this.timeOut);
         }
         this.timeOut = setTimeout(() => {
-          this.$store.dispatch('loadData', this.username);
-          this.$store.commit('setUser', this.username);
+          this.$store.dispatch('loadData', e.target.value);
         }, 500);
       }
     }
@@ -70,6 +68,7 @@ export default {
 }
 
 .dropdown-list {
+  background-color: #f5faff;
   margin-top: 0;
   text-align: left;
   list-style-type: none;
